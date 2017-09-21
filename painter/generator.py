ef11f7e8a58d4dc411820e8dfe77256bb285d2cd
alex-klim <goldencrane@ukr.net>
2017-09-21 10:26:16 +0000

    finally had a deal with generator

--- painter/generator.py
+++ painter/generator.py
@@ -1,31 +1,96 @@
 import collections as cl
+from fs import *
 
 
-def cli(dict_or_func):
+def cli(submenu):
     while True:
-        print(type(dict_or_func))
+        isLeaf = False
+        print(submenu['prompt'])
 
-        cmd = input("input one of these %s: "
-        % ','.join(dict_or_func))
+        for item in submenu.items():
+            if item[0] == 'func':
 
-        print("cmd is: ", cmd)
-
-        if(isinstance(dict_or_func[cmd], dict)):
-            next(cli(dict_or_func[cmd]))
-        else:
-            yield cmd
+                item[1]()
+                submenu = menu
+                isLeaf = True
         
+        if isLeaf == False:
+            cmd = input()
+            for i, item in enumerate(submenu['siblings']):
+                if item['name'] == cmd:
+                    submenu =  item
+
+        yield submenu['name']
+        
+def draw_circle():
+    pass
+
+def draw_rectangle():
+    pass
+
+def draw():
+    pass
+
 def exit():
     print("shutting down")
+
+def open(ftype='json'):
+    tmp = {}
+    if ftype == 'json':
+        tmp = load_json()
+        print(tmp)
+    else:
+        tmp = load_pickle()
+        print(tmp)
     
-def enter_filename():
-    print("i'm entering filename")
+def save():
+    print('func save')
 
 siblings = cl.namedtuple('siblings', ['subs'])
 
-commands = {
-    'open' : {'siblings': siblings(subs=('enter_filename')),
-    'enter_filename': enter_filename},
-    'draw' : {},
-    'exit' : exit,
+menu = {
+    'name' : 'root',
+    'prompt' : 'Choose your destiny',
+    'siblings' : [
+            {   
+                'name' : 'open',
+                'prompt' : 'enter filename',
+                'func' : open,
+                'epilogue' : 'file opened'
+            },
+            {
+                'name' : 'save',
+                'prompt' : 'enter filename',
+                'func' : save,
+                'epilogue' : 'file saved'
+            },
+            {
+                'name' : 'add',
+                'prompt' : 'which figure you wanna add?(circle, rectangle)',
+                'siblings' : [
+                    {
+                    'name' : 'circle',
+                    'prompt' : 'enter x, y, radius',
+                    'func' : draw_circle,
+                    'epilogue' : 'circle added to image'
+                    },
+                    {
+                    'name' : 'rectangle',
+                    'prompt' : 'enter x, y, side1, side2',
+                    'func' : draw_rectangle,
+                    'epilogue' : 'circle added to image'
+                    }
+                ]
+                },
+            {
+                'name' : 'draw',
+                'prompt' : 'start drawing',
+                'func' : draw
+            },
+            {
+                'name' : 'exit',
+                'prompt' : 'bye!',
+                'func' : exit
+            }
+    ]
 }
--- painter/index.py
+++ painter/index.py
@@ -1,7 +1,7 @@
 import generator
 
 if __name__ =="__main__":
-    cmd = generator.cli(generator.commands)
+    cmd = generator.cli(generator.menu)
     while True:
         command = next(cmd)
         print("from main: ", command)
